cfcea3417c8d3bcc6e65a8da2fea33b07d60bab9
PVieira04 <vieira.pjpv@gmail.com>
2023-03-20 16:48:19 +0000

    add backbone to html and add simple function to JS for test.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,11 @@
-alert("hello")
+const welcome = () => {
+    document.getElementById('div-head').textContent = 'Welcome to the quiz just for Film Fanatics!';
+    document.getElementById('div-text').textContent = 'You will be given five random questions and each one will give you four options to choose from. Once you select your answer, We will tell you if it was correct, and you can move on to the next question (you cannot go back to previous questions). Once you have answered your fifth question, you can view the results!';
+    document.getElementById('next').textContent = 'Start Game'
+}
+
+const displayQuestion = () => {
+    
+}
+
+welcome();
