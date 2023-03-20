3e31303ffb0befb0f4fd1d94e23751fff9af222a
PVieira04 <vieira.pjpv@gmail.com>
2023-03-20 23:19:53 +0000

    add question three

--- assets/main.js
+++ assets/main.js
@@ -68,7 +68,38 @@ const displayQuestionTwo = () => {
 }
 
 const displayQuestionThree = () => {
-    alert('you want to go to question three!')
+    divHead.textContent = 'Question 3';
+    divText.textContent = `In the Star Wars universe, on what planet did Luke Skywalker grow up on?`;
+    topRow.innerHTML = `<button id='q1' class='option'>Mandalore</button><button id='q2' class='option'>Toydaria</button>`;
+    botRow.innerHTML = `<button id='q3' class='option'>Tatooine</button><button id='q4' class='option'>Alderaan</button>`;
+    feedback.textContent = '';
+    next.innerHTML = '';
+    let answered = false;
+    let buttons = document.getElementsByClassName('option');
+    for (let button of buttons) {
+        button.addEventListener('click', function highlightSelection() {
+            if (answered === true) {
+                return
+            }
+            else if (button.textContent === document.getElementById('q3').textContent) {
+                this.style.backgroundColor = 'green';
+                feedback.textContent = 'Correct!';
+            }
+            else {
+                this.style.backgroundColor = 'red';
+                document.getElementById('feedback').textContent = `Sorry! That is incorrect. The correct answer is ${document.getElementById('q3').textContent}.`;
+            }
+            answered = true;
+            next.innerHTML = `<button id='next-button'>Next Question</button>`;
+            document.getElementById('next-button').addEventListener('click', function() {
+                displayQuestionFour();
+            })
+        })
+    }
+}
+
+const displayQuestionFour = () => {
+    alert('you want q4!')
 }
 
 document.addEventListener('DOMContentLoaded', () => {
